8ae01bd2555eb0204d8c9f06f64267180717a32b
alisoam <ali.sorouramini@gmail.com>
2022-11-08 19:31:20 +0330

    Add python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "dcloader",
+    version = "0.0.0",
+    description = "",
+    author = "Ali Sorour Amini",
+    author_email = "ali.sorouramini@gmail.com",
+    url = "https://github.com/alisoam/dcloader",
+)
